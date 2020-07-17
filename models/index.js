41825cbf7a3ca59226d1c2596faa58b86787d347
imseongtae <dev.haemil@gmail.com>
2020-07-18 07:00:35 +0900

    chore: Add .env and dotenv module to use environment variable

--- models/index.js
+++ models/index.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+// use dotenv
+require('dotenv').config();
 
 const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '12345',
-	database: 'opentutorials',
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
 });
 db.connect();
 
