b432a5a22a0a31880b2617a6ae66821ffae037a0
imseongtae <dev.haemil@gmail.com>
2020-07-20 17:42:02 +0900

    reafactor: Do Refactoring to separate query statement

--- library/topic.js
+++ library/topic.js
@@ -7,22 +7,7 @@ const db = require('./../models');
 const template = require('./template');
 
 const topicTable = require('../models/topic');
-
-// const home = response => {
-// 	db.query(`SELECT * FROM topic`, (error, topics) => {
-// 		const title = 'Welcome';
-// 		const description = 'Hello Nodejs-MySQL';
-// 		const list = template.list(topics);
-// 		const html = template.HTML(
-// 			title,
-// 			list,
-// 			`<h2>${title}</h2>${description}`,
-// 			`<a href="/create">create</a>`,
-// 		);
-// 		response.writeHead(200);
-// 		response.end(html);
-// 	});
-// };
+const authorTable = require('../models/author');
 
 const home = async response => {
 	try {
@@ -45,54 +30,13 @@ const home = async response => {
 	}
 };
 
-// const page = (request, response) => {
-// 	const _url = request.url;
-// 	const queryData = url.parse(_url, true).query;
-
-// 	db.query('SELECT * from topic', (error, topics) => {
-// 		if (error) throw error;
-// 		db.query(
-// 			`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id = ?`,
-// 			[queryData.id], // 공격의 의도가 있는 코드는 세탁해서 처리해줌
-// 			(error2, topic) => {
-// 				if (error2) throw error2;
-// 				console.log(topic);
-// 				// sanitizeHtml는 악성 스크립트를 방어하기 위한 패키지 모듈임
-// 				const sanitizedTitle = sanitizeHtml(topic[0].title);
-// 				const sanitizedDescription = sanitizeHtml(topic[0].description, {
-// 					allowedTags: ['h1'],
-// 				});
-// 				const list = template.list(topics);
-// 				const html = template.HTML(
-// 					sanitizedTitle,
-// 					list,
-// 					`
-//             <h2>${sanitizedTitle}</h2>${sanitizedDescription}
-//             <p>by ${topic[0].name}</p>
-//           `,
-// 					`
-//             <a href="/create">create</a>
-//             <a href="/update?id=${queryData.id}">update</a>
-//             <form action="delete_process" method="post">
-//               <input type="hidden" name="id" value="${queryData.id}">
-//               <input type="submit" value="delete">
-//             </form>
-//           `,
-// 				);
-// 				response.writeHead(200);
-// 				response.end(html);
-// 			},
-// 		);
-// 	});
-// };
-
 const page = async (request, response) => {
 	const _url = request.url;
 	const queryData = url.parse(_url, true).query;
 
 	try {
 		const topics = await topicTable.getTopics();
-		if (topics.length == 0)
+		if (topics.length === 0)
 			throw { status: 404, erorrMessage: 'not found topics' };
 		const [topic] = await topicTable.getTopics({ id: queryData.id });
 		if (!topic) {
@@ -126,64 +70,70 @@ const page = async (request, response) => {
 	}
 };
 
-const create = response => {
-	db.query(`SELECT * FROM topic`, (error, topics) => {
-		if (error) throw error;
-		db.query(`SELECT * FROM author`, (error2, authors) => {
-			// sanitizeHtml는 악성 스크립트를 방어하기 위한 패키지 모듈임
-			const title = 'Create';
-			const list = template.list(topics);
-			const html = template.HTML(
-				title,
-				list,
-				`
-          <h2>${title}</h2>
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              ${template.selectAuthor(authors)}							
-            </p>
-            <p>
-              <input type="submit" value="create post">
-            </p>
-          </form>
-        `,
-				`<a href="/create">create</a>`,
-			);
-			response.writeHead(200);
-			response.end(html);
-		});
-	});
+const create = async response => {
+	try {
+		const topics = await topicTable.getTopics();
+		if (topics === 0) throw { status: 404, erorrMessage: 'not found topics' };
+		const authors = await authorTable.getAuthors();
+
+		const title = 'Create';
+		const list = template.list(topics);
+		const html = template.HTML(
+			title,
+			list,
+			`
+				<h2>${title}</h2>
+				<form action="/create_process" method="post">
+					<p><input type="text" name="title" placeholder="title"></p>
+					<p>
+						<textarea name="description" placeholder="description"></textarea>
+					</p>
+					<p>
+						${template.selectAuthor(authors)}
+					</p>
+					<p>
+						<input type="submit" value="create post">
+					</p>
+				</form>
+			`,
+			`<a href="/create">create</a>`,
+		);
+		response.writeHead(200);
+		response.end(html);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
-const create_process = (request, response) => {
-	let body = '';
-	request.on('data', function (data) {
-		body += data;
-	});
-	request.on('end', function () {
-		const post = qs.parse(body);
-		// sanitizeHtml는 악성 스크립트를 방어하기 위한 패키지 모듈임
-		// 그러므로 sanitizeHtml 을 사용자로부터 값을 입력받는 부분에 걸어야 함!
-		const sanitizedTitle = sanitizeHtml(post.title);
-		const sanitizedDescription = sanitizeHtml(post.description, {
-			allowedTags: ['h1'],
+const create_process = async (request, response) => {
+	try {
+		let body = '';
+		request.on('data', function (data) {
+			body += data;
 		});
-		db.query(
-			`INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`,
-			// [post.title, post.description, post.author],
-			[sanitizedTitle, sanitizedDescription, post.author],
-			(error, result) => {
-				if (error) throw error;
-				// 글 생성 후 해당 글로 이동!
-				response.writeHead(302, { Location: `/?id=${result.insertId}` });
-				response.end();
-			},
-		);
-	});
+		request.on('end', async function () {
+			const post = qs.parse(body);
+			// sanitizeHtml는 악성 스크립트를 방어하기 위한 패키지 모듈임
+			// sanitizeHtml 을 사용자로부터 값을 입력받는 부분에 적용
+			const sanitizedTitle = sanitizeHtml(post.title);
+			const sanitizedDescription = sanitizeHtml(post.description, {
+				allowedTags: ['h1'],
+			});
+
+			const newTopic = {
+				title: sanitizedTitle,
+				description: sanitizedDescription,
+				author_id: post.author,
+			};
+			// const result = await topicTable.create(connection, newTopic);
+			const result = await topicTable.insert(newTopic);
+			console.log('결과: ', result); // result 가 반환하는 insertId
+			response.writeHead(302, { Location: `/?id=${result.insertId}` });
+			response.end();
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const update = (request, response) => {
